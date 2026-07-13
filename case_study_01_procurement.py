49cc1feae3e6b8f02f5f0167fac42affd6bd9f21
Taeyeon Kim <taekim1291@gmail.com>
2026-07-13 14:57:01 -0600

    case study

--- case_study_01_procurement.py
+++ case_study_01_procurement.py
@@ -16,4 +16,20 @@ print(po_df.Total_Cost.max())
 
 # Which vendors made the most purchase orders?
 high_value_orders = po_df["Total_Cost"] > 50000
+high_value_purchase_orders = po_df[high_value_orders]
 result_vendor = po_df[high_value_orders].groupby('Vendor')
+
+# vendor_counts = result_vendor.size()
+# print(vendor_counts.sort_values())
+
+# Creating another CSV file
+high_value_purchase_orders.to_csv("high_value_POs.csv", index=False)
+vendor_purchase_order_counts = (
+    result_vendor,
+    high_value_purchase_orders
+    .groupby('Vendor')
+    .size()
+    .sort_values(ascending=False),
+)
+
+print(vendor_purchase_order_counts)
